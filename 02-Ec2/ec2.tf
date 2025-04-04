c4ff6fc8e5b28376046fd125b2f16c97477273fc
YashavanthaN8057 <yme1061920@gmail.com>
2025-04-04 17:05:40 +0530

    learning terraform ~ auto push

--- 02-Ec2/ec2.tf
+++ 02-Ec2/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "main" {
-  # ami           = "ami-0fcc78c828f9df2"
-  # ami-0fcc78c828f981df2
-  ami           = "ami-072983368f2a6eab5"
+   ami           = "ami-0fcc78c828f9df2"
+ 
   instance_type = "t3.micro"
+  
 
   tags = {
     Name = "backend"
