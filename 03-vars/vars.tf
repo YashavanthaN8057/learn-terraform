ff28c9b30b1136aacdc438278741fdcc9b487a0a
YashavanthaN8057 <yme1061920@gmail.com>
2025-04-05 11:43:33 +0530

    learining terraform - auto push

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -10,9 +10,11 @@ variable "salary" {
   default = 10 # This is how you define a variable with default value 
 }
 
-output "Prinitng_salary" {   # This is how you print an output variable
+output "Prinitng_salary" {   # This is how you print an output variable and rule is that output variable name 
+# should be in lowercase and underscore and not in camelcase
+  # and the value should be in double quotes and it should be in lowercase and underscore and not in camelcase
+
   value = "The salary of the employee is ${var.salary}" 
-  
   
   # This is how we can access a variable
 }
