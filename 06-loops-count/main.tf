1610fc0f48a962737017a70085d975e17373e2b7
YashavanthaN8057 <yme1061920@gmail.com>
2025-04-07 15:07:04 +0530

    learining terraform - auto push

---
+++ 06-loops-count/main.tf
@@ -0,0 +1,16 @@
+resource "null_resource" "main" {
+  count = 3
+}
+
+variable "stocks" {
+  default = ["Infy, TCS, Wipro"]
+}
+
+resource "null_resource" "this" {
+  count = length(var.stocks)
+}
+
+resource "null_resource" "stocks" {
+    count = length(var.stocks)
+    
+    }
