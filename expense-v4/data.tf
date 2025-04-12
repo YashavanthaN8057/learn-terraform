1e522dbf72ea4e25dbbb3882182d59af6b60a999
YashavanthaN8057 <yme1061920@gmail.com>
2025-04-13 00:35:37 +0530

    learining terraform - auto push

---
+++ expense-v4/data.tf
@@ -0,0 +1,17 @@
+data "aws_route53_zone" "main" {
+  name         = "cloudcera.shop"
+  private_zone = false
+}
+
+data "aws_security_group" "main" {
+  name = "b59-custom-secgrp"
+}
+
+data "aws_ami" "main" {
+  most_recent = true
+
+  owners = ["034362053751"]
+  tags = {
+    Name = "DevOps-LabImage-RHEL9"
+  }
+}
---
+++ expense-v4/vars.tf
@@ -0,0 +1,28 @@
+variable "componets" {
+  default = {
+    mysql = {
+      instance_type = "t3.medium"
+
+    }
+    backend = {
+      instance_type = "t2.micro"
+    }
+
+    frontend = {
+      instance_type = "t3.micro"
+    }
+  }
+}
+
+variable "env" {}
+
+# variable "ami" {
+#   default = "ami-0fcc78c828f981df2"
+# }
+
+# variable "vpc_security_group_ids" {
+#   default = ["sg-0b37bb6b6f027ffc5"]
+# }
+# variable "zone_id" {
+#     default = "Z08061862LBZAM174JIHO"
+# }
